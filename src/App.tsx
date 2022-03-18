682c02b844460c30f457bb11a9e94a625aa57ca6
Anna-Maria Auer <anna-maria.auer@capgemini.com>
2022-03-18 11:38:12 +0100

    conditional rendering

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import JSX from './chapters/02_jsx_tsx/JSX';
 import ExampleChapter from './chapters/99_example/ExampleChapter';
 import RevealWrapper from './components/reveal/RevealWrapper';
 import React from 'react';
+import ConditionalRendering from './chapters/03_conditional_rendering/ConditionalRendering';
 
 const App: React.FC = () => {
   return (
@@ -9,6 +10,7 @@ const App: React.FC = () => {
       <ExampleChapter />
       {/* more Chapters to come here... */}
       <JSX />
+      <ConditionalRendering />
     </RevealWrapper>
   );
 };
